85ee2c5e5cf96117c5d400c960b0f2ae21564587
RFish ⚓ <RonanCherrueau@gmail.com>
2018-12-13 17:25:27 +0100

    Requirements and Setup section

---
+++ ptech18/setup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# Setup the lab
+sudo snap install microstack --classic --edge
+sudo snap install openstackclients --classic --edge
---
+++ ptech18/teardown.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Teardown the lab
+for vm in $(openstack server list -c Name -f value); do\
+  echo "Deleting ${vm}...";\
+  openstack server delete "${vm}";\
+done
+
+for ip in $(openstack floating ip list -c "Floating IP Address" -f value); do\
+  echo "Delete ${ip}...";\
+  openstack floating ip delete "${ip}";\
+done
+
+sudo snap remove openstackclients
+sudo snap remove microstack
