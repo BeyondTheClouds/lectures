cd99577742aaa24c3ad35f528a55381943cac9b8
RFish ⚓ <RonanCherrueau@gmail.com>
2019-02-26 09:52:53 +0100

    [2019/os-imt] Day 2 of the lab

--- 2019/os-imt/teardown.sh
+++ 2019/os-imt/teardown.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 . admin-openrc.sh
 
+# Delete VMs
+for vm in $(openstack server list -c Name -f value); do \
+  echo "Deleting ${vm}..."; \
+  openstack server delete "${vm}"; \
+done
 
+# Releasing floating IPs
+for ip in $(openstack floating ip list -c "Floating IP Address" -f value); do \
+  echo "Releasing ${ip}..."; \
+  openstack floating ip delete "${ip}"; \
+done
 
 sudo snap remove openstackclients
 sudo snap remove microstack
