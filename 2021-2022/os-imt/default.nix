95baef58afbf1bc6c4cadbaf3458cb4607dafaac
Marie-Donnie <delavergnemarie@gmail.com>
2021-11-16 11:29:47 +0100

    This year

---
+++ 2021-2022/os-imt/default.nix
@@ -0,0 +1,70 @@
+with import <nixpkgs> {};
+
+let packageOverrides = self: super: {
+    # Override python-language-server at top level to make it effective
+    # also for for isort and mypy
+    python-language-server = super.python-language-server.override {
+      # Only make few features available
+      # https://github.com/NixOS/nixpkgs/blob/e5a015bd85f6d1578702aaa7e710b028775c76aa/pkgs/development/python-modules/python-language-server/default.nix
+      providers = [
+        "pycodestyle"  # Style linting (Ex-pep8)
+        "pyflakes"     # Error linting (no style)
+      ];
+    };
+  };
+  py-dev = (python38.override {inherit packageOverrides;}).withPackages (py-pkg: [
+                py-pkg.python-language-server
+                py-pkg.pyls-isort
+                py-pkg.pyls-mypy
+            ]);
+in mkShell {
+  buildInputs = [ py-dev python2 pipenv libffi openssl ];
+  shellHook = ''
+    # Set SOURCE_DATE_EPOCH so that we can use python wheels
+    SOURCE_DATE_EPOCH=$(date +%s)
+
+    # Set PYTHONPATH to empty to fix `pipenv install`
+    PYTHONPATH=
+
+    # Mount /bin in the tmpfs so that I can symlink bash into /bin
+    # without messing my nix config
+    #
+    # http://blog.programster.org/overlayfs/
+    # https://news.ycombinator.com/item?id=5024654
+    # E.g, protect your home:
+    # > mount -t overlay -o lowerdir=~/,upperdir=/tmp/tmpHOME\
+    # > my-overlay ~/
+    # - lowerdir: Files in lowerdir appear in the overlay
+    # - upperdir: where creation/modification are stored
+    # - workdir: Needs to be an empty directory on the same fs as
+    #   upperdir (merge of lower/upper)
+    # - my-overlay: Name of the overlay for latter umount (e.g. `sudo
+    #   umount my-overlay`)
+    # - Any file created or changed in overlay appear
+    #   in the upper dir
+    OVERLAY_NAME="bin-bash-overlay$(pwd | sed -e 's|/|-|g')"
+    function teardown() {
+      echo "INFO: umount $OVERLAY_NAME."
+      sudo umount "$OVERLAY_NAME"
+    }
+
+    if ! fgrep -q '$OVERLAY_NAME on /bin' <<< $(mount -l)
+    then
+      echo "INFO: Kolla/tools uses #!/bin/bash not linked in NixOS"
+      echo "INFO: Make an overlayfs on /bin and link bash in /bin"
+      echo "INFO: mount $OVERLAY_NAME."
+
+      UPP_BIN="$(mktemp -d)"
+      WORK_BIN="$(mktemp -d)"
+
+      sudo mount --types overlay --options \
+        lowerdir=/bin,upperdir=$UPP_BIN,workdir=$WORK_BIN \
+        "$OVERLAY_NAME" /bin
+
+      ln --symbolic --verbose /run/current-system/sw/bin/bash /bin/bash
+    fi
+
+    # teardown on CTRL+d
+    trap "teardown" exit
+  '';
+}
---
+++ 2021-2022/os-imt/setup-infra/microstack/setup.py
@@ -0,0 +1,161 @@
+# $ pipenv run setup  [--test/--no-test]
+
+import logging
+import socket
+import itertools
+
+import click
+
+from enoslib.types import Host, Roles
+from enoslib.api import (play_on, ensure_python3)
+from enoslib.infra.enos_g5k.provider import G5k
+from enoslib.infra.enos_g5k.configuration import (Configuration)
+
+
+logging.basicConfig(level=logging.DEBUG)
+LOG = logging.getLogger(__name__)
+TEAMS = [
+    ("Ronan", "Adrien"),
+    # ("Thomas Herbelin", "Simon Le Bars"),
+    # ("Samy Le Galloudec", "Marc Lamy"),
+    # ("Maël Gaonach", "Antoine Omond"),
+    # ("Robin Carrez", "Evrard-Nil Daillet"),
+    # ("Paul Bavazzano", "Hoan My TRAN"),
+    # ("Florian Le Menn", "" ),
+    # ("Xavier Aleman", "Ismaïl El Khantache"),
+    # ("Barthélémy Tek", ""),
+    # ("Javed  Syed Khasim", "Emeric Chonigbaum"),
+    # ("Lucas Pelec", ""),
+    # ("Théo Coutant", ""),
+    # ("Raphael Hascoet", ""),
+    # ("", ""),
+    # ("", "")
+    # ("ronana",      "alebre"),
+    # ("alacour",     "mnoritop"),
+    # ("aqueiros",    "rlao"),
+    # ("blemee",      "launea"),
+    # ("bpeyresaube", "nguegan"),
+    # ("cg",          "mmainchai"),
+    # ("damarti",     "eguerin"),
+    # ("jfreta",      "vjorda"),
+    # ("kforest",     "maguyo"),
+    # ("lparis",      "sedahmani"),
+    # ("mmaheo",      "qeud"),
+    # ("nfrayssinhe", "thlailler"),
+    # ("rgrison",     "tandrieu"),
+]
+
+
+def get_ip_addr(h: Host) -> str:
+    'Returns the IP address of a Host `h`'
+    try:
+        return f'{socket.gethostbyname(h.address)}, {h.address}'
+    except socket.gaierror:
+        return h.address
+
+
+def make_conf(testing=True) -> Configuration:
+    conf = {
+        "reservation": "2021-03-05 07:00:01",
+        "walltime": "11:59:58",
+        "job_name": "lab-2021-imta-fise-login-os",
+        "env_name": "ubuntu2004-x64-min",
+        "project": "lab-2021-imta-fise-login-os",
+        "resources": {
+            "networks": [
+                {
+                    "id": "net",
+                    "type": "prod",
+                    "roles": ["network_interface"],
+                    "site": "rennes",
+                },
+                # Keep this for future work, for a deployment
+                # based OpenStack.
+                # {
+                #     # Note: *NEVER* assigns this to a machine!
+                #     "id": "net_ext",
+                #     "type": "slash_22",
+                #     "roles": ["neutron_external_interface"],
+                #     "site": "rennes",
+                # },
+            ],
+            "machines": [
+                {
+                    "roles": ["OpenStack"],
+                    "cluster": "paravance",
+                    "nodes": 13,
+                    "primary_network": "net",
+                    "secondary_networks": [ ],
+                }
+            ],
+        }
+    }
+
+    if testing:
+        del(conf["reservation"])
+        conf["walltime"] = "07:00:00"
+        conf["job_name"] = "test-lab-2021-imta-fise-login-os"
+        conf["resources"]["machines"][0]["nodes"] = 1
+
+    return Configuration.from_dictionnary(conf)
+
+
+def provision(rs: Roles):
+    ensure_python3(roles=rs)
+
+    with play_on(roles=rs, pattern_hosts="OpenStack") as p:
+        # Install the bare necessities
+        p.apt(pkg=['bat', 'curl', 'htop', 'tcpdump', 'lynx', 'vim', 'kmod'],
+              update_cache=True)
+        # Workaround ripgrep error
+        # https://bugs.launchpad.net/ubuntu/+source/rust-bat/+bug/1868517
+        p.raw('apt download ripgrep')
+        p.raw('dpkg --force-overwrite -i ripgrep*.deb')
+
+        # IP Forwarding
+        p.raw('sysctl -w net.ipv4.ip_forward=1')
+
+        # Setup ssh for root w/ password
+        p.raw('echo "root:lab-os" | chpasswd')
+        p.blockinfile(path='/etc/ssh/sshd_config',
+                      block='''
+                      PasswordAuthentication yes
+                      PermitRootLogin yes
+                      ''')
+        p.systemd(name='ssh', state='restarted')
+
+        # Enhance default bash
+        for l in ('. /etc/bash_completion',         # Offer bash completion
+                  'export PATH=/snap/bin:${PATH}',  # Put /snap/bin in PATH
+                  'alias cat="bat --style=plain"',    # Better cat
+                  'alias fgrep="rg --fixed-strings"'  # Better fgrep
+                  ) :
+            p.lineinfile(path='/root/.bashrc', line=l)
+
+
+
+# Main
+@click.command()
+@click.option('--test/--no-test', default=False)
+def main(test):
+    # Claim the resources
+    infra = G5k(make_conf(testing=test))
+    roles, networks = infra.init(force_deploy=False)
+
+    # Provision machines
+    provision(roles)
+
+    # Display subnet
+    subnet = [n for n in networks
+                if "neutron_external_interface" in n["roles"]]
+    print(subnet)
+
+    # Assign machines
+    print("Lab machine assignations:")
+    addrs = map(get_ip_addr, roles["OpenStack"])
+    for (usr, addr) in zip_longest(TEAMS, addrs):
+        print(f"- {addr} ({usr}): ")
+
+
+if __name__ == "__main__":
+    main()
